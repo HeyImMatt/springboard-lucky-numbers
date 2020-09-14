02fdc13d51b68bc3e92410ecd299ad9caa82f6be
heyimmatt <mrmcfarland@gmail.com>
2020-09-14 15:57:27 -0700

    Display error messages or facts on the page

--- lucky-nums/static/lucky.js
+++ lucky-nums/static/lucky.js
@@ -8,15 +8,34 @@ async function processForm(evt) {
   const color = $("#color").val();
 
   const resp = await axios.post("/api/get-lucky-num", {name, year, email, color});
-  handleResponse(resp);
+  handleResponse(resp.data);
 }
 
 /** handleResponse: deal with response from our lucky-num API. */
 
 function handleResponse(resp) {
-  console.log(resp)
+  const errors = resp.errors
+  const $nameErr = $("#name-err")
+  const $yearErr = $("#year-err")
+  const $emailErr = $("#email-err")
+  const $colorErr = $("#color-err")
+
+  $nameErr.text('');
+  $yearErr.text('');
+  $emailErr.text('');
+  $colorErr.text('');
+
+  if (errors) {
+    $nameErr.text(errors.name)
+    $yearErr.text(errors.year)
+    $emailErr.text(errors.email)
+    $colorErr.text(errors.color)
+  } else {
+    const num = resp.num;
+    const year = resp.year;
+    $("#lucky-results").text(`Your lucky number is ${num.num} (${num.fact}).
+    Your birth year (${year.year}) fact is ${year.fact}`)
+  }
 }
-
-
 
 $("#lucky-form").on("submit", processForm);
